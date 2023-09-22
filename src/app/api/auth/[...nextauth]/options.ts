b9779eb86d4ca954baa2569328c9b013f793e0e2
Nick Robinson <nick@imnickrobinson.com>
2023-09-22 10:17:48 -0400

    Adding auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,27 @@
+import type { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = { id: 1, name: "J Smith", email: "test"};
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        })
+    ]
+}
--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Alert, ProgressBar, Modal, Button, Stack} from "react-bootstrap";
+import { Alert, ProgressBar, Modal, Button, Stack, Form} from "react-bootstrap";
 
 export const metadata = {
     title: 'Submit Your Prop Sheet',
@@ -17,13 +17,46 @@ export default function Page() {
             <Modal.Header>
             <Stack gap={1}>
                 <div><h4>Question 4</h4></div>
-                <div><h4>Section: National Anthem Time</h4></div>
+                <div><h4>Section: Game Points</h4></div>
             </Stack>
             </Modal.Header>
 
 
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          <p>1st Quarter <b>9.5 Seconds</b></p>
+        <Form>
+          <Form.Check 
+            inline
+            type="radio"
+            name="group1"
+            id={`default-radio`}
+            label={`Over`}
+          />
+          <Form.Check 
+            inline
+            type="radio"
+            name="group1"
+            id={`default-radio`}
+            label={`Under`}
+            />
+        </Form>
+        <p>2nd Quarter <b>13.5 Seconds</b></p>
+        <Form>
+          <Form.Check 
+            inline
+            type="radio"
+            name="group1"
+            id={`default-radio`}
+            label={`Over`}
+          />
+          <Form.Check 
+            inline
+            type="radio"
+            name="group1"
+            id={`default-radio`}
+            label={`Under`}
+            />
+        </Form>
         </Modal.Body>
 
         <Modal.Footer>
