beb7e56f60f6aac96ae12b132fa3ebebedc3d356
Nick_Robinson <Nick.Robinson-c@mnx.com>
2024-02-05 18:56:55 -0500

    More logging

--- src/app/components/group-form.tsx
+++ src/app/components/group-form.tsx
@@ -81,7 +81,7 @@ export default function GroupForm() {
         } catch (error) {
             console.log(JSON.stringify(error));
             console.error('Failed to fetch data from the API - group creation', error);
-            setError('Error calling API' + error);
+            setError('Error calling API' + JSON.stringify(error));
         }
         setIsLoading(false);
     };
